6dfca33a7dc4e63774b910600e7685b8d8117102
Eugene <the_gabber@mail.ru>
2019-12-12 23:57:58 +0300

    Fix for LE example

---
+++ example_ie/dso.cpp
@@ -0,0 +1,3 @@
+thread_local int t_glob = 0;
+thread_local int t_glob_with_val = 5;
+
--- example_ie/main.cpp
+++ example_ie/main.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-extern void fn();
-
-thread_local int t_glob = 0;
-thread_local int t_glob_with_val = 5;
+extern thread_local int t_glob;
+extern thread_local int t_glob_with_val;
 
 int main(int argc, char** argv)
 {
-	fn();
+	t_glob++;
+	t_glob_with_val += 2;
 	printf("Glob %d glob_with_val %d\n", t_glob, t_glob_with_val);
 	return 0;
 }
